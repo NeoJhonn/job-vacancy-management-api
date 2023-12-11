e54b6aec54f1512424f364502626ef283a58d18a
NeoJhonn <jhonny.azevedo@gmail.com>
2023-12-10 21:30:58 -0300

    CandidateRepository and Endpoint create() created

--- src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
@@ -1,21 +1,27 @@
 package br.com.jhonnyazevedo.job_vacancy_management.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jhonnyazevedo.job_vacancy_management.entities.Candidate;
+import br.com.jhonnyazevedo.job_vacancy_management.repositories.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateRepository repository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody Candidate candidate) {
-    System.out.println("Candidato");
-    System.out.println(candidate.getEmail());
-  }
-  
+  public Candidate create(@Valid @RequestBody Candidate candidate) {
+    this.repository.save(candidate);
+
+    return candidate;
+}
+
 }
