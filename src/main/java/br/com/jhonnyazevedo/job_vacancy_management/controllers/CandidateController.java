41f3408e1d1ba0755d5ae12c748b3dbc76ac71cb
NeoJhonn <jhonny.azevedo@gmail.com>
2023-12-11 23:34:56 -0300

    Validating existing User/Candidate

--- src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
@@ -1,13 +1,14 @@
 package br.com.jhonnyazevedo.job_vacancy_management.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jhonnyazevedo.job_vacancy_management.entities.Candidate;
-import br.com.jhonnyazevedo.job_vacancy_management.repositories.CandidateRepository;
+import br.com.jhonnyazevedo.job_vacancy_management.services.CreateCandidateService;
 import jakarta.validation.Valid;
 
 @RestController
@@ -15,13 +16,19 @@ import jakarta.validation.Valid;
 public class CandidateController {
 
   @Autowired
-  private CandidateRepository repository;
+  private CreateCandidateService service;
 
   @PostMapping("/")
-  public Candidate create(@Valid @RequestBody Candidate candidate) {
-    this.repository.save(candidate);
+  public ResponseEntity<Object> create(@Valid @RequestBody Candidate candidate) {
 
-    return candidate;
+    try {
+      var result = this.service.execute(candidate);
+      return ResponseEntity.ok().body(result);
+      
+    } catch(Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    
 }
 
 }
--- src/main/java/br/com/jhonnyazevedo/job_vacancy_management/repositories/CandidateRepository.java
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/repositories/CandidateRepository.java
@@ -1,5 +1,6 @@
 package br.com.jhonnyazevedo.job_vacancy_management.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.jhonnyazevedo.job_vacancy_management.entities.Candidate;
 
 public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
-  
+
+
+  Optional<Candidate> findByUsernameOrEmail(String username, String email);
 }
