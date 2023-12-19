cd9155f3759bcea4f5bd12cd9feb55b7bb93138d
NeoJhonn <jhonny.azevedo@gmail.com>
2023-12-19 00:02:18 -0300

    Company Controller created

---
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CompanyController.java
@@ -0,0 +1,33 @@
+package br.com.jhonnyazevedo.job_vacancy_management.controllers;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jhonnyazevedo.job_vacancy_management.entities.Company;
+import br.com.jhonnyazevedo.job_vacancy_management.services.CreateCompanyService;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+  @Autowired
+  private CreateCompanyService service;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody Company company) {
+
+    try {
+      var result =  this.service.execute(company);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+  
+}
