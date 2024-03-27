4d3e6a8e05814b61035e8969c54d81b01896cdf0
NeoJhonn <jhonny.azevedo@gmail.com>
2024-03-27 01:02:05 -0300

    CandidateProfile route created

--- src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/controllers/CandidateController.java
@@ -1,15 +1,16 @@
 package br.com.jhonnyazevedo.job_vacancy_management.controllers;
 
+import br.com.jhonnyazevedo.job_vacancy_management.services.ProfileCandidateService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.jhonnyazevedo.job_vacancy_management.entities.Candidate;
 import br.com.jhonnyazevedo.job_vacancy_management.services.CandidateService;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
@@ -17,6 +18,9 @@ public class CandidateController {
 
   @Autowired
   private CandidateService service;
+
+  @Autowired
+  private ProfileCandidateService profileCandidateService;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody Candidate candidate) {
@@ -28,7 +32,20 @@ public class CandidateController {
     } catch(Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
-    
-}
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<Object> get(HttpServletRequest request) {
+
+    var idCandidate = request.getAttribute("candidate_id");
+
+    try {
+      var profile = this.profileCandidateService.execute(UUID.fromString(idCandidate.toString()));
+      return ResponseEntity.ok().body(profile);
+
+    } catch(Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
 
 }
---
+++ src/main/java/br/com/jhonnyazevedo/job_vacancy_management/services/ProfileCandidateService.java
@@ -0,0 +1,50 @@
+package br.com.jhonnyazevedo.job_vacancy_management.services;
+
+import br.com.jhonnyazevedo.job_vacancy_management.dtos.ProfileCandidateResponseDTO;
+import br.com.jhonnyazevedo.job_vacancy_management.repositories.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ProfileCandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+
+        var candidate = this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("User not found.");
+                });
+
+        var candidateDTO = ProfileCandidateResponseDTO.builder()
+                .description(candidate.getDescription())
+                .username(candidate.getUsername())
+                .email(candidate.getEmail())
+                .name(candidate.getName())
+                .id(candidate.getId())
+                .build();
+
+
+        return candidateDTO;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
